e0f92490b85d9827b49fd733ef304186c0920eeb
Ridho Hertanto <ridhoho2000@gmail.com>
2026-01-10 13:29:12 +0700

    feat(DisplayHistoryBigRoad): first commit to build display history big road
    Add tile and squaretiles to display history big road. First feature added, more update incoming (incomplete).

---
+++ src/DisplayHistoryBigRoad.jsx
@@ -0,0 +1,57 @@
+import "./DisplayHistoryBigRoad.css";
+
+function DisplayHistoryBigRoad() {
+  return (
+    <>
+      <div className="tile">
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+        <div className="squaretiles"></div>
+      </div>
+    </>
+  );
+}
+
+export default DisplayHistoryBigRoad;
